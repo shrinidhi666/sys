f726e0086e7548ed89d6b7b093d1a2389d52eed3
root <root@blue0666.bluepixels.in>
2013-12-31 07:33:40 +0530

    fixing

--- clone/makeClone.sh
+++ clone/makeClone.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 mount /boot                                                                                                                                                                                                                        
-rm -fr /crap/gentooOnesis.bak.old                                                                                                                                                                                                            
-mv /crap/gentooOnesis.bak /crap/gentooOnesis.bak.old                                                                                                                                                                                         
-mv /crap/gentooOnesis /crap/gentooOnesis.bak                                                                                                                                                                                                 
+rm -frv /crap/gentooOnesis                                                                                                                                                                                                  
 mkdir /crap/gentooOnesis        
 
-/home/shrinidhi/bin/gitHub/sys/clone/makeClone.py --root / -t /crap/gentooOnesis/  -l /blueprod/STOR1/CRAP.serv/:/crap/STOR1.crap,/blueprod/STOR1/backup1/:/crap/incoming -b -x proc,dev,sys,blueprod,mnt,/crap,/BACKUP/,run,/etc/udev/rules.d/,tmp,home,.config/,.pulse/,.pulse-cookie,/etc/conf.d/hostname,etc/mtab -m sys:root,home:root,tmp:root,/etc/udev/rules.d/:root,run:root,/dev/:root,/proc/:root,/crap:root,mnt/cdrom:root,mnt/livecd:root,/crap/LOCAL.crap:root%0777 -e var/tmp,var/log
+/home/shrinidhi/bin/gitHub/sys/clone/makeClone.py --root / -t /crap/gentooOnesis/  -l /blueprod/STOR1/CRAP.serv/:/crap/STOR1.crap,/blueprod/STOR1/backup1/:/crap/incoming -b -x proc,dev,sys,blueprod,mnt,/crap,/BACKUP/,run,/etc/udev/rules.d/,tmp,home,.config/,root/,.pulse/,.pulse-cookie,/etc/conf.d/hostname,etc/mtab -m root:root,sys:root,home:root,tmp:root,/etc/udev/rules.d/:root,run:root,/dev/:root,/proc/:root,/crap:root,mnt/cdrom:root,mnt/livecd:root,/crap/LOCAL.crap:root%0777 -e var/tmp,var/log
+rsync -av /crap/gentooOnesis/etc/skel/ /crap/gentooOnesis/root/ ; chown root:root /crap/gentooOnesis/root/ -Rv
 
 
 
